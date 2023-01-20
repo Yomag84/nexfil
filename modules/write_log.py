b6394e5cb5995c8064dfea04ba51106cd4bd7ed7
Yomag84 <yomag84@gmail.com>
2023-01-20 15:04:16 +0100

    (Solved): ValueError: Unrecognised argument(s): encoding

--- modules/write_log.py
+++ modules/write_log.py
@@ -4,8 +4,10 @@ import modules.share
 
 def log_writer(message):
     logging.basicConfig(
-        filename=modules.share.LOG_FILE_PATH,
-        encoding='utf-8',
+        handlers=[
+            logging.FileHandler(
+                filename=modules.share.LOG_FILE_PATH,
+                encoding='utf-8', mode='a+')],
         level=logging.DEBUG,
         format='[%(asctime)s] : %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p'
